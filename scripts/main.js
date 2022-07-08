44a8496a5acd5edf9419ea4d56a84de8f243e1a6
James Subedi <jyotindra.subedi@ebpearls.com>
2022-07-08 15:54:21 +0545

    cv experience updated

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,7 @@ AOS.init({
   anchorPlacement: 'top-left',
   duration: 1000
 });
+var ageInMilliseconds = new Date() - new Date("1998-07-24");
+let myAge =  Math.floor(ageInMilliseconds/1000/60/60/24/365); 
+document.getElementById("age").innerHTML = myAge;
 
-// Add your javascript here
